758e5029c022fda5ddd27f055f6c49660cf5184b
buise <1078688845@qq.com>
2017-08-06 12:15:25 +0800

    fh

---
+++ js/history.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Administrator on 2017/8/4.
+ */
+$(function () {
+    $('.search').click(function () {
+        $('.click').toggle();
+    })
+});
---
+++ js/index.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Administrator on 2017/8/4.
+ */
+// $(function () {
+//     $('.con-bottom-right').click(function () {
+//
+//     })
+// })
